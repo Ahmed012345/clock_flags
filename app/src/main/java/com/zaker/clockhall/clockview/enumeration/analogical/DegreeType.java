614e5e8776918fc2f19233c1703d1853cf8304f7
AhmedSaad <krarooo281@gmail.com>
2021-05-10 01:36:08 +0200

    Add files via upload

---
+++ app/src/main/java/com/zaker/clockhall/clockview/enumeration/analogical/DegreeType.java
@@ -0,0 +1,23 @@
+package com.zaker.clockhall.clockview.enumeration.analogical;
+
+public enum DegreeType {
+
+    line(0), circle(1), square(2);
+
+    DegreeType(int i) {
+        this.id = i;
+    }
+
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public static DegreeType fromId(int id) {
+        for (DegreeType clockDegreeType : values()) {
+            if (clockDegreeType.id == id) return clockDegreeType;
+        }
+        throw new IllegalArgumentException();
+    }
+}
